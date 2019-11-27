f3228b179ba38e8e37f0bf9bf174dc77aac0dec2
Tom Wood <tom.wood@civicactions.com>
2019-11-27 16:21:40 -0500

    Remove --rename option; some refactoring

--- secrender/secrender.py
+++ secrender/secrender.py
@@ -28,9 +28,6 @@ import os, os.path, datetime
               type=click.Path(exists=True, dir_okay=False, readable=True),
               required=True,
               help='Template (Jinja2)')
-@click.option('--rename', '-r', nargs=2,
-              multiple=True,
-              help='-r YKEY VAR will define a Jinja2 variable VAR based on the value of YKEY in the YAML file.')
 @click.option('--root', '-R',
               help='If supplied, names a Jinja2 variable that will hold all the top-level YAML values.')
 @click.option('--set', '-s', 'set_', nargs=2,
@@ -43,32 +40,17 @@ import os, os.path, datetime
 @click.option('--output-dir', '-O', 'output_dir',
               type=click.Path(exists=True, file_okay=False, dir_okay=True),
               help='Default output directory')
-def main(in_, template_path, rename, root, set_, output_file, output_dir):   
-    output_path = make_output_path(output_file, output_dir)
-
+def main(in_, template_path, root, set_, output_file, output_dir):   
     with open(in_, "r") as stream:
         yaml = load(stream, Loader=FullLoader)
 
+    template_args = get_template_args(yaml, root, set_)
+    output_path = make_output_path(output_file, output_dir)
+
+    secrender(template_path, template_args, output_path)
+
+def secrender(template_path, template_args, output_path):
     template = get_template(template_path)
-
-    template_args = dict()
-
-    if root:
-        target = dict()
-        template_args[root] = target
-    else:
-        target = template_args
-        
-    rename_dict = dict((k, v) for (k, v) in rename)
-
-    for key, value in yaml.items():
-        key = rename_dict.get(key, key)
-        target[key] = value
-        
-    for (key, value) in set_:
-        template_args[key] = value
-        
-    template_args['current_date'] = datetime.datetime.today()
     rendered = template.render(**template_args)
 
     with open(output_path, 'w') as output:
@@ -90,6 +72,25 @@ def make_output_path(output_file, output_dir):
         return output_file
     else:
         return os.path.join(output_dir, output_file)
+
+def get_template_args(yaml, root, set_):
+    template_args = dict()
+
+    if root:
+        target = dict()
+        template_args[root] = target
+    else:
+        target = template_args
+        
+    for key, value in yaml.items():
+        target[key] = value
+        
+    for (key, value) in set_:
+        template_args[key] = value
+        
+    template_args['current_date'] = datetime.datetime.today()
+
+    return template_args
 
 if __name__ == '__main__':
     main()
