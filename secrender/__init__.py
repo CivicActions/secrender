d9c2125679f127386e731f4a491dda0e5c30cd01
Tom Wood <tom.wood@civicactions.com>
2020-01-09 16:35:00 -0500

    Packaging work (#10)
    * Move version number to secrender/__init__.py and add a requirements.in
    
    Fix requirements.in
    
    * Add requirements.in and requirements.txt to manifest
    
    * Add a setup.cfg so we can try out some release automation

---
+++ secrender/__init__.py
@@ -0,0 +1,5 @@
+from .secrender import secrender
+from .secrender import get_template_args
+
+__all__ = [ 'secrender', 'get_template_args' ]
+__version__ = '0.5.0'
--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import setup
+import re
+from setuptools import setup, find_packages
 
+with open('requirements.in', 'r') as fd:
+    requirements = fd.read().strip().split('\n')
+
+with open('secrender/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+    
 setup(
     name='secrender',
     description='Render Jinja2 templates using YAML file for substitutions.',
-    version='0.1',
+    version=version,
     author='Tom Wood',
     author_email='tom.wood@civicactions.com',
+    url='http://github.com/CivicActions/secrender',
     license='CC',
-    packages=['secrender'],
+    packages=find_packages(),
     zip_safe=False,
-    install_requires=[
-        'click',
-        'pyyaml',
-        'pyyaml-include',
-        'Jinja2',
-    ],
+    install_requires=requirements,
     entry_points = {
         'console_scripts': ['secrender=secrender.secrender:main']
     }
