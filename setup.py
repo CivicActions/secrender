e0b8e9ad92bca1f94b999354a5579e87994fb1cf
Tom Wood <tom.wood@civicactions.com>
2020-01-16 10:11:59 -0500

    Add some pytest tests; apply some PEP8 fixes; try out poetry for packaging; update CI pipeline.X

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 import re
 from setuptools import setup, find_packages
 
-with open('requirements.in', 'r') as fd:
-    requirements = fd.read().strip().split('\n')
+with open("requirements.in", "r") as fd:
+    requirements = fd.read().strip().split("\n")
 
-with open('secrender/__init__.py', 'r') as fd:
+with open("secrender/__init__.py", "r") as fd:
     version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        fd.read(),
-        re.MULTILINE
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
     ).group(1)
-    
+
 setup(
-    name='secrender',
-    description='Render Jinja2 templates using YAML file for substitutions.',
+    name="secrender",
+    description="Render Jinja2 templates using YAML file for substitutions.",
     version=version,
-    author='Tom Wood',
-    author_email='tom.wood@civicactions.com',
-    url='http://github.com/CivicActions/secrender',
-    license='CC',
+    author="Tom Wood",
+    author_email="tom.wood@civicactions.com",
+    url="http://github.com/CivicActions/secrender",
+    license="CC",
     packages=find_packages(),
     zip_safe=False,
     install_requires=requirements,
-    entry_points = {
-        'console_scripts': ['secrender=secrender.secrender:main']
-    }
+    entry_points={"console_scripts": ["secrender=secrender.secrender:main"]},
 )
